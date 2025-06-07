dc2e95a3b4c36728995ccf66b1375a27a3ab36e9
Cornerstone-04 <fortunecornerstone@gmail.com>
2025-06-07 10:08:06 +0100

    feat: add scroll to top

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -5,15 +5,17 @@ import {
   Header,
   Hero,
 } from "@/components/landing";
+import { ScrollToTop } from "@/components/shared/scroll-to-top";
 
 export default function LandingPage() {
   return (
-    <div className="min-h-screen bg-white dark:bg-zinc-900 text-zinc-900 dark:text-white flex flex-col">
+    <div className="relative min-h-screen bg-white dark:bg-zinc-900 text-zinc-900 dark:text-white flex flex-col">
       <Header />
       <Hero />
       <Features />
       <FAQSection />
       <Footer />
+      <ScrollToTop/>
     </div>
   );
 }
